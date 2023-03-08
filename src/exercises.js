1ccbf81455ac0e97049b64aac3f58204132daa83
Sergio-Cano <sergio95cano@hotmail.com>
2023-03-08 23:34:06 +0100

    Ejercicio finalizado

--- src/exercises.js
+++ src/exercises.js
@@ -1,15 +1,24 @@
-function filterNumbers(arr, largerThan) {
+/* function filterNumbers(arr, largerThan) {
   return arr.filter(function (number) {
     return number > largerThan;
   });
-}
+} */
 
-function returnFirstTwoDrivers() {
+const filterNumbers = (arr, largerThan) => arr.filter(n => n > largerThan);
+
+
+/* function returnFirstTwoDrivers() {
   const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
   return drivers.slice(0, 2);
-}
+} */
 
-const person = {
+const returnFirstTwoDrivers = () => {
+  const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
+  return drivers.slice(0,2);
+} 
+  
+
+/* const person = {
   name: "Bob",
   greet: function () {
     setTimeout(
@@ -20,40 +29,90 @@ const person = {
       2000
     );
   },
-};
+}; */
 
-function add(a, b) {
+const person = {
+  name: "Bob",
+  greet: function() {
+    setTimeout( () => console.log(`Hello, my name is ${this.name}`),2000)
+  }
+}
+
+//person.greet();
+
+
+/* function add(a, b) {
   a = a || 1;
   b = b || 1;
   return a + b;
+} */
+
+function add(a = 1, b = 1){
+  return a + b;
 }
 
-function createGrid(array) {
+
+/* function createGrid(array) {
   return `Generates a ${array[0]} x ${array[1]} grid`;
 }
+*/
 
-function sum() {
+function createGrid([width, height]) {
+  return `Generates a ${width} x ${height} grid`;
+}
+
+
+
+/* function sum() {
   const argsArray = Array.prototype.slice.call(arguments);
   return argsArray.reduce(function (a, b) {
     return a + b;
   }, 0);
+} */
+
+function sum() {
+  const argsArray = Array.prototype.slice.call(arguments);
+  return argsArray.reduce(function (a, ...args) {
+    return a + args;
+  }, 0);
 }
 
-function addNumbers(numbers) {
+
+
+/* function addNumbers(numbers) {
+  return numbers.reduce(function (sum, number) {
+    return sum + number;
+  }, 0);
+} */
+
+function addNumbers(...numbers) {
   return numbers.reduce(function (sum, number) {
     return sum + number;
   }, 0);
 }
 
-function calculate(product, numbers) {
+
+/* function calculate(product, numbers) {
+  return numbers.map(function (number) {
+    return number * product;
+  });
+} */
+
+function calculate(product, ...numbers) {
   return numbers.map(function (number) {
     return number * product;
   });
 }
 
-function join(array1, array2) {
+
+/* function join(array1, array2) {
   return array1.concat(array2);
-}
+} */
+
+function join(array1, ...array2) {
+  return array1.concat(array2);
+} 
+
 
 const savedFile = {
   extension: "jpg",
@@ -61,14 +120,24 @@ const savedFile = {
   size: 14040,
 };
 
-function fileSummary(file) {
+/* function fileSummary(file) {
   return (
     "The file" + file.name + "." + file.extension + "is of size" + file.size
   );
+} */
+
+function fileSummary(file) {
+  return (
+    `The file ${file.name}.${file.extension} is of size ${file.size}`
+  );
 }
+
+
 
 function addressMaker(address) {
   // Add some code here
+
+  const { city , state } = address;
 
   const newAddress = {
     city: address.city,
@@ -76,10 +145,10 @@ function addressMaker(address) {
     country: "United States",
   };
 
-  return `${newAddress.city}, ${newAddress.state}, ${newAddress.country}`;
+  return `${city}, ${state}, ${newAddress.country}`;
 }
 
-function Ninja(options) {
+/* function Ninja(options) {
   this.name = options.name;
   this.health = 100;
   this.speed = 3;
@@ -106,4 +175,22 @@ Ninja.prototype.showStats = function () {
 Ninja.prototype.drinkSake = function () {
   this.health += 10;
   console.log("Health: " + this.health);
-};
+}; */
+
+class Ninja {
+  name = "";
+  health = 100;
+  speed = 3;
+  strength = 3;
+
+  constructor(name){
+    this.name = name;
+  }
+
+  sayName = () => console.log(`My ninja name is ${this.name}`);
+  showStats = () => console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+  drinkSake = function () {
+    this.health += 10;
+    console.log(`Health: ${this.health}`)
+  }
+}
